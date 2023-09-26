a0535b3fed3b0d6e05fed8919b306143c35dc500
Dreamkaman <riabushko@gmail.com>
2023-09-26 11:41:35 +0300

    Start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import index files block
+import './index.html'; //for 'html-loader'
+import './partials/header.html';
+import './partials/footer.html';
+import './partials/hero.html';
+import './partials/section-1.html';
+import './index.scss'; //for 'mini-css-extract-plugin'
